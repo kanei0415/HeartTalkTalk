bf805a604e5ad22c5f62466c8053324c3eef4c7d
K4NEI <kimyoungho0415@gmail.com>
2024-05-12 17:41:31 +0900

    feat/black: updated

--- functions/functions/src/modules/chattings/prompts/prompts.interface.ts
+++ functions/functions/src/modules/chattings/prompts/prompts.interface.ts
@@ -9,6 +9,7 @@ export const PROMPTS = {
   resultMessagePrompt: "RESULT_MESSAGE_PROMPT",
   titleMessagePrompt: "TITLE_MESSAGE_PROMPT",
   responseMessagePrompt: "RESPONSE_MESSAGE_PROMPT",
+  serveyResultMessagePrompt: "SERVEY_RESULT_MESSAGE_PROMPT",
 } as const;
 
 export type PromptType = (typeof PROMPTS)[keyof typeof PROMPTS];
@@ -18,4 +19,5 @@ export const DEFAULT_PROMPTS = {
   [PROMPTS.resultMessagePrompt]: "You are a psychology counselor, and you should explain the conclusion of the patient's symptoms and how to deal with them. You must response with korean to your patients. The following are some of the conversations the patient had during the consultation. The conversation consists of JSON arrangements, and the contents of each element are the actual conversation. If the value of the sender key is SYSTEM, it is the counselor's question, and if it is USER, it is the patient's answer. What follows is the patient's characteristics. What follows should not be included in the question",
   [PROMPTS.titleMessagePrompt]: "You are a psychological counselor and need to define the exact psychological disease name for the patient's symptoms. You have to answer your patient's disease name in Korean. The answer should be in one word. For example, 'panic disorder' or 'depression' are the conversations the patient had during counseling. The conversation consists of a JSON arrangement, and the content of each element is the actual conversation. If the value of the source key is SYSTEM, it is the counselor's question, and if it is USER, it is the patient's answer. Here are the characteristics of the patient. Here are some of the things that should not be included in the question.",
   [PROMPTS.responseMessagePrompt]: "As a psychological counselor, you have to ask questions about psychological symptoms and to know exactly where the patient feels psychological pain in the patient's answers. You have to ask questions in Korean, and what follows should be the patient's characteristics, previous questions, and answers. What follows should not be included in the question.",
+  [PROMPTS.serveyResultMessagePrompt]: "You're a psychological counselor, and you need to find out the cause in a patient suffering from psychological pain and tell them how to improve it. Ask the patient again questions. They should last until he realizes his or her psychological condition. You must response with korean to your patients. and the following is the config of the patient.",
 };
--- functions/functions/src/modules/users/user.interface.ts
+++ functions/functions/src/modules/users/user.interface.ts
@@ -8,4 +8,5 @@ export interface FireStoreUserType {
   config: string;
   createdAt: number;
   reservedDays: number;
+  id?: string;
 }
